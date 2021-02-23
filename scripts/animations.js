553eabbb8020a0e5f91d64f8b45774cd06b5b2ec
Andradev101 <contato.santosti@gmail.com>
2021-02-23 20:42:19 -0300

    minor styling changes

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,57 +1,30 @@
-/*video bg changes after 40 seconds*/
-var video = document.querySelector('video');
-var source = document.querySelector('source');
-var v = ["/resources/videos/1.mp4#t=7", "/resources/videos/2.mp4#t=20", "/resources/videos/3.mp4#t=12", "/resources/videos/4.mp4#t=15,40" ]
-var bgcolor = document.getElementsByClassName("bg-color");
+/*video bg changes after 25 seconds*/
+const video = document.querySelector('video');
+const source = document.querySelector('source');
+const v = ["/resources/videos/0.mp4#t=7", "/resources/videos/1.mp4#t=20", "/resources/videos/2.mp4#t=12", "/resources/videos/3.mp4#t=15,40" ]
+const bgcolor = document.getElementsByClassName("bg-color");
+const modeldiv = document.querySelectorAll(".model");
+const modeldesc = document.getElementsByClassName("desc");
 
-setInterval(() => {
-    bgcolor[0].style.backdropFilter="blur(0px)";
-}, 1000);
-
-
-for (let index = 0; index < v.length ; index++) {    
-    setTimeout(() => {
-        source.setAttribute('src', v[index]);
-        video.load();
-        console.log(index);
-    }, index * (30*1000));
-}
-
-/*
-var video = document.querySelector('video');
-var source = document.querySelector('source');
-var counter = 0;
-var v = ["/resources/videos/1.mp4#t=7", "/resources/videos/2.mp4#t=20"]
-
-
-
-
-
-
-source.setAttribute('src', v[counter]);
-video.load();
-video.appendChild(source);
-
-setTimeout(function() {  
-    counter++
-    source.setAttribute('src', v[counter]);
-    video.load();
-}, 1000);
-
-
-----------------------------------------------------------------------------
-var video = document.querySelector('video');
-var source = document.querySelector('source');
-var v = ["/resources/videos/1.mp4#t=7", "/resources/videos/2.mp4#t=20"]
-
-for (let index = 0; index < v.length; index++) {
-    source.setAttribute('src', v[index])
-    video.load();
-    
     setInterval(() => {
-        source.setAttribute('src', v[index]);
+        bgcolor[0].style.backgroundColor=" rgba(0, 0, 0, 0.7)"
+    }, 1000);
+    
+    function addVideo(){
+        var random = Math.floor(Math.random() * 4);
+        source.setAttribute('src', v[random]);
         video.load();
-    }, 3000);
-}
+    }
+    addVideo()
 
-*/
+    setInterval(() => {
+        var random = Math.floor(Math.random() * 4);
+        source.setAttribute('src', v[random]);
+        video.load();
+    }, 25*1000);
+
+    for (var i = 0; i < modeldiv.length; ++i) {
+        var item = modeldiv[i];
+        var delay = i*0.1;
+        modeldiv[i].style.animationDelay=delay+"s"
+    }
